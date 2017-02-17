43f7f7eaa766bbe51a72bd247c94e6408b8152fc
Beth <bethbrod@gmail.com>
2017-02-17 02:18:51 +0000

    Initial commit

--- highest_number_cubed/main.py
+++ highest_number_cubed/main.py
@@ -1,5 +1,12 @@
 """This is the entry point of the program."""
 
-
 def highest_number_cubed(limit):
-    pass
+    previous_number = 1
+    
+    while True:
+        current_number = previous_number + 1
+        if current_number ** 3 >= limit:
+            return previous_number #when loop ends, set previous number as current to restart loop
+        
+        previous_number = current_number
+        
